a2d1d3c8b9fb9872817e6709545166a7c1f2a0bb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-15 22:01:11 +0200

    Update page.tsx

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,3 +1,6 @@
+import VerifyEmail from "@/components/VerifyEmail";
+import Image from "next/image";
+
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined;
@@ -6,15 +9,40 @@ interface PageProps {
 
 const VerifyEmailPage = ({ searchParams }: PageProps) => {
   const token = searchParams.token;
-  console.log("VerifyEmailPage  token:", token);
-  return (
-    <div className="container relative flex pt-20 flex-col item-center justify-center lg:px-0">
-      <div className="mx-auto flex w-full justify-center space-y-6 sm:w-[350px]">
-        {token && typeof token === "string" ? (
-          <div className="grid gap-6">
+  const toEmail = searchParams.to;
+  const email = searchParams.email;
 
+  return (
+    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        {(token && typeof token === "string") && (email && typeof email === "string") ? (
+          <div className="grid gap-6">
+            <VerifyEmail token={token} email={email} />
           </div>
-        ) : null}
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+              <Image
+                src="/hippo-email-sent.png"
+                fill
+                alt="hippo email sent image"
+              />
+            </div>
+
+            <h3 className="font-semibold text-2xl">Check your email</h3>
+
+            {toEmail ? (
+              <p className="text-muted-foreground text-center">
+                We&apos;ve sent a verification link to{" "}
+                <span className="font-semibold">{toEmail}</span>.
+              </p>
+            ) : (
+              <p className="text-muted-foreground text-center">
+                We&apos;ve sent a verification link to your email.
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
