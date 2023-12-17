aefa01e7430a855608d9a1254a5558a668a4feb9
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-17 21:41:21 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
       >
         <main className="relative flex flex-col min-h-screen">
           <Providers>
-          {/* @ts-expect-error Async Server Component */}
             <Navbar />
             <div className="flex-grow flex-1">{children}</div>
           </Providers>
