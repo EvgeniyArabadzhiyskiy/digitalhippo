8d49e62e72902d87719531b951bc3cd27e309e5f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-18 22:40:50 +0200

    Update Users.ts

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,37 @@
-import { CollectionConfig } from "payload/types";
+import { User } from "@/payload-types";
+import { CollectionConfig, FieldAccess ,Access } from "payload/types";
+
+export const adminsAndUser: Access<any, User> = ({ req: { user }}) => {
+  // console.log("user:", user);
+  if (user) {
+    const isAdmin = user?.role === "admin";
+
+    if (isAdmin) {
+      return true;
+    }
+
+    const resultObject = {
+      id: {
+        equals: user.id,
+      },
+    }
+    // console.log("resultObject============", resultObject);
+
+    return resultObject
+  }
+
+  return false;
+};
+
+export const isAdmin: Access<any, User> = ({ req: { user }}) => {
+  const isAdmin = user?.role === "admin";
+  return isAdmin;
+};
+ 
+export const isAdminFieldLevel: FieldAccess<{ id: string }, unknown, User> = ({ req: { user }}) => {
+  const isAdmin = user?.role === 'admin';
+  return isAdmin;
+};
 
 export const Users: CollectionConfig = {
   slug: "users",
@@ -10,10 +43,12 @@ export const Users: CollectionConfig = {
     },
   },
   access: {
-    // read: () => true,
-    // create: () => true,
-    create: ({ req }) => req.user.role === "admin",
-    update: ({ req }) => req.user.role === "admin",
+    read: adminsAndUser,
+    create: isAdmin,  // create:() => true,
+
+    update: adminsAndUser, // isAdmin
+    delete: isAdmin,
+    
     // delete: ({ req }) => req.user.role === "admin",
   },
   fields: [
@@ -21,12 +56,22 @@ export const Users: CollectionConfig = {
       name: "role",
       defaultValue: "user",
       required: true,
-        // admin: {
-        //   condition: (data, sec, {user}: {user: Partial<User>}) => {
-        //     const isAdmin = user.role === "admin";
-        //     return false
-        //   },
+      // admin: {
+      //   condition: (data, sec, {user}: {user: Partial<User>}) => {
+      //     const isAdmin = user.role === "admin";
+      //     return false
+      //   },
+      // },
+
+      access: {
+        // create: ({ req }: {req: PayloadRequest<User>}) => {
+        //   const user = req.user?.role
+        //   return true;
         // },
+        // create: isAdminFieldLevel,
+        update: isAdminFieldLevel,
+        read: () => true,
+      },
       type: "select",
       options: [
         { label: "Admin", value: "admin" },
@@ -34,5 +79,22 @@ export const Users: CollectionConfig = {
         { label: "Guest", value: "guest" },
       ],
     },
+
+    {
+      name: "sites",
+      type: "relationship",
+      relationTo: "sites",
+      access: {
+        create: isAdminFieldLevel,
+        update: isAdminFieldLevel,
+      },
+      // admin: {
+      //   condition: (args) => {
+      //       const ssr = args
+      //       // console.log("ssr:", ssr);
+      //     return true
+      //   }
+      // }
+    }
   ],
 };
