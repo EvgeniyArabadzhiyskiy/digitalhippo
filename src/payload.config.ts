32e0bc5726c728409befcb988ecce4d24242f878
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-20 21:13:31 +0200

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -18,7 +18,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Sites, Media],
+  collections: [Users, Products, Sites, Media, Pages],
   routes: {
     admin: "/sell",
   },
