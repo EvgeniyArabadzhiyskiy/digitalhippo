6c85779178cbfad77dc3f63d7c65c28cf12721c4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-29 21:36:05 +0200

    Update ProductListing.tsx

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Product } from "@/payload-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Skeleton } from "./ui/skeleton";
+import Link from "next/link";
+import { cn, formatPrice } from "@/lib/utils";
+import { PRODUCT_CATEGORIES } from "@/config";
+import ImageSlider from "./ImageSlider";
 
 interface ProductListingProps {
   product: Product | null;
@@ -13,18 +17,58 @@ const ProductPlaceholder = () => {
   return (
     <div className="flex flex-col w-full">
       <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl">
-        <Skeleton />
+        <Skeleton className="w-full h-full bg-slate-500" />
       </div>
+      <Skeleton className="mt-4 w-2/3 h-4 rounded-lg bg-slate-500" />
+      <Skeleton className="mt-2 w-16 h-4 rounded-lg bg-slate-500" />
+      <Skeleton className="mt-2 w-12 h-4 rounded-lg bg-slate-500" />
     </div>
   );
 };
 
 const ProductListing = ({ product, index }: ProductListingProps) => {
+  // console.log("ProductListing  index:", index);
   const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, index * 75);
+
+    return () => {
+      // console.log("UNMOuNT");
+
+      clearTimeout(timer);
+    };
+  }, [index]);
 
   if (!product || !isVisible) return <ProductPlaceholder />;
 
-  //   return <></>;
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label;
+
+  if (product && isVisible) {
+    return (
+      // <Link
+      //   className={cn("invisible h-full w-full cursor-pointer group/main", {
+      //     "visible animate-in fade-in-5": isVisible,
+      //   })}
+      //   href={`/product/${product.id}`}
+      // >
+        <div className="flex flex-col w-full">
+          <ImageSlider urls={['']}/>
+          <h3 className="mt-4 font-medium text-sm text-gray-700">
+            {product.name}
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{label}</p>
+          <p className="mt-1 font-medium text-sm text-gray-900">
+            {formatPrice(product.price)}
+          </p>
+        </div>
+      // </Link>
+    );
+  }
 };
 
 export default ProductListing;
