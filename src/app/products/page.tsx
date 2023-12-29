1ff0579b72d8c6595977c101893254030ed742bd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-29 21:36:01 +0200

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,5 @@
 import SomeComponent from "@/components/UserAccountNav";
+import payload from "payload";
 
 const getPost = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
@@ -10,12 +11,16 @@ const getPost = async () => {
 };
 
 const Page = async () => {
-  const posts = await getPost();
+  // const posts = await getPost();
+  const {docs} = await payload.find({
+    collection: "products"
+  })
+  // console.log("Page  docs:", docs[1]);
   return (
     <>
       <h1>Products Page</h1>
-      {posts.title}
-      <SomeComponent />
+     Title: {docs[1].name}
+      {/* <SomeComponent /> */}
     </>
   );
 };
