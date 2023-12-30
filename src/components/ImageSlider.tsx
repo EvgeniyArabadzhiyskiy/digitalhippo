150e105a7231c21e1796242b972e96b80b359477
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-30 22:14:26 +0200

    Create ImageSlider.tsx

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,126 @@
+import { useEffect, useState } from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import 'swiper/css/keyboard';
+import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import type SwiperType from "swiper";
+import { Navigation, Pagination, Keyboard, Scrollbar } from "swiper/modules";
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface ImageSliderProps {
+  urls: string[];
+}
+
+const Buttons = () => {
+  const swiper = useSwiper();
+  const activeStyle =
+    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+  const inactiveStyle = "hidden text-gray-400";
+  return (
+    <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
+      <button
+        className={activeStyle}
+        onClick={() => console.log("ImageSlider  swiper:", swiper.slideNext())}
+      >
+        &rarr;
+      </button>
+      {/* <button onClick={() => swiper.slidePrev()}>&larr;</button> */}
+    </div>
+  );
+};
+
+const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+  // const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: swiper?.isEnd
+    // isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+  
+
+  // useEffect(() => {
+  //   swiper?.on("slideChange", ({activeIndex}) => {
+  //     setActiveIndex(activeIndex)
+
+  //     setSlideConfig({
+  //       isBeginning: activeIndex === 0,
+  //       isEnd: activeIndex === (urls.length ?? 0) - 1
+  //     })
+  //   })
+  // },[swiper, urls.length])
+
+  const activeStyle =
+    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+  const inactiveStyle = "hidden text-gray-400";
+
+  return (
+    <div className="group relative bg-zinc-500 aspect-square overflow-hidden rounded-xl">
+      <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
+
+        <button 
+          className={cn(activeStyle, "right-3 transition", { 
+            [inactiveStyle]: slideConfig.isEnd,
+            "hover:bg-blue-300 text-blue-800 opacity-100": !slideConfig.isEnd,
+          })} 
+          onClick={() => swiper?.slideNext(500)}
+          aria-label="next-image"
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />
+        </button>
+
+        <button 
+          className={cn(activeStyle, "left-3 transition", { 
+            [inactiveStyle]: slideConfig.isBeginning,
+            "hover:bg-blue-300 text-blue-800 opacity-100": !slideConfig.isBeginning,
+          })}
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev(500)
+          }}
+          aria-label="previous-image"
+        >
+          <ChevronLeft className="h-4 w-4 text-zinc-700" />
+        </button>
+      </div>
+      
+      <Swiper
+        className="h-full w-full"
+        onSwiper={(swiper) => setSwiper(swiper)}
+        onSlideChange={({ activeIndex, isBeginning, isEnd }) => {
+          // setActiveIndex(activeIndex);
+
+          setSlideConfig({
+            isBeginning: isBeginning,
+            isEnd: isEnd,
+          });
+        }}
+        modules={[Pagination, Navigation, Keyboard, Scrollbar]}
+        // scrollbar={{
+        //   el: '.swiper-scrollbar',
+        //   draggable: true
+        // }}
+        // keyboard={{
+        //   enabled:true
+        // }}
+        slidesPerView={1}
+        spaceBetween={50}
+      >
+        {urls.map((item) => {
+          return (
+            <SwiperSlide key={item}>
+              <div className="flex justify-center items-center h-[200px] bg-red-300">
+                Slide {item}
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageSlider;
