c17502ba20f8907c3c1cdefa5fa9c28035b6fde0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-12-31 22:40:45 +0200

    Update ImageSlider.tsx

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/pagination";
-import 'swiper/css/keyboard';
-import 'swiper/css/scrollbar';
+import "swiper/css/keyboard";
+import "swiper/css/scrollbar";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import type SwiperType from "swiper";
 import { Navigation, Pagination, Keyboard, Scrollbar } from "swiper/modules";
 import { cn } from "@/lib/utils";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
 
 interface ImageSliderProps {
   urls: string[];
@@ -16,7 +17,7 @@ interface ImageSliderProps {
 const Buttons = () => {
   const swiper = useSwiper();
   const activeStyle =
-    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+    "active:scale-[0.97] grid  hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
   const inactiveStyle = "hidden text-gray-400";
   return (
     <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
@@ -37,10 +38,9 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
 
   const [slideConfig, setSlideConfig] = useState({
     isBeginning: true,
-    isEnd: swiper?.isEnd
+    isEnd: swiper?.isEnd,
     // isEnd: activeIndex === (urls.length ?? 0) - 1,
   });
-  
 
   // useEffect(() => {
   //   swiper?.on("slideChange", ({activeIndex}) => {
@@ -54,39 +54,38 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   // },[swiper, urls.length])
 
   const activeStyle =
-    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+    "active:scale-[0.97] grid hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
   const inactiveStyle = "hidden text-gray-400";
 
   return (
     <div className="group relative bg-zinc-500 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-
-        <button 
-          className={cn(activeStyle, "right-3 transition", { 
+        <button
+          className={cn(activeStyle, "right-3 transition", {
             [inactiveStyle]: slideConfig.isEnd,
-            "hover:bg-blue-300 text-blue-800 opacity-100": !slideConfig.isEnd,
-          })} 
+            "hover:bg-blue-300 text-blue-800": !slideConfig.isEnd,
+          })}
           onClick={() => swiper?.slideNext(500)}
           aria-label="next-image"
         >
           <ChevronRight className="h-4 w-4 text-zinc-700" />
         </button>
 
-        <button 
-          className={cn(activeStyle, "left-3 transition", { 
+        <button
+          className={cn(activeStyle, "left-3 transition", {
             [inactiveStyle]: slideConfig.isBeginning,
-            "hover:bg-blue-300 text-blue-800 opacity-100": !slideConfig.isBeginning,
+            "hover:bg-blue-300 text-blue-800": !slideConfig.isBeginning,
           })}
           onClick={(e) => {
             e.preventDefault();
-            swiper?.slidePrev(500)
+            swiper?.slidePrev(500);
           }}
           aria-label="previous-image"
         >
           <ChevronLeft className="h-4 w-4 text-zinc-700" />
         </button>
       </div>
-      
+
       <Swiper
         className="h-full w-full"
         onSwiper={(swiper) => setSwiper(swiper)}
@@ -94,8 +93,8 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
           // setActiveIndex(activeIndex);
 
           setSlideConfig({
-            isBeginning: isBeginning,
-            isEnd: isEnd,
+            isBeginning,
+            isEnd,
           });
         }}
         modules={[Pagination, Navigation, Keyboard, Scrollbar]}
@@ -106,15 +105,25 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
         // keyboard={{
         //   enabled:true
         // }}
+
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="${className}"></span>`
+          }
+        }}
         slidesPerView={1}
         spaceBetween={50}
       >
-        {urls.map((item) => {
+        {urls.map((url) => {
           return (
-            <SwiperSlide key={item}>
-              <div className="flex justify-center items-center h-[200px] bg-red-300">
-                Slide {item}
-              </div>
+            <SwiperSlide key={url} className="-z-10 h-full w-full">
+              <Image
+                src={url}
+                alt="Product image"
+                fill
+                loading="eager"
+                className="-z-10 h-full w-full object-cover object-center"
+              />
             </SwiperSlide>
           );
         })}
