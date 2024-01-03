db14b28163dce3f84b28fb162f417019f5a364ab
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-03 18:51:42 +0200

    Delete route.ts

--- src/app/api/products/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function DELETE(req: Request, res: any) {
-  const id = res.params.id
-  // const { id } = params;
-  console.log("DELETE  id:", id);
-
-  return NextResponse.json({ message: id });
-}
