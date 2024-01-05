4fb3673a50bb1d12a8609fda04d2ccb395012359
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-05 20:27:46 +0200

    Update page.tsx

--- src/app/test-1/page.tsx
+++ src/app/test-1/page.tsx
@@ -1,5 +1,4 @@
 import InnerTest1 from "@/components/InnerTest1";
-import { SyncedWithArticles } from "@/components/SyncedWithArticles";
 import { getPayloadClient } from "@/get-payload";
 import getQueryClient from "@/lib/helpers/getQueryClient";
 import { Hydrate, dehydrate } from "@tanstack/react-query";
@@ -15,26 +14,20 @@ const getProducts = async () => {
   return items;
 };
 
-const Test1 = async  () => {
+
+
+const Test1 = async () => {
   const queryClient = getQueryClient();
 
   await queryClient.prefetchQuery([["getProducts"], { type: "query" }], getProducts);
 
   const dehydratedState = dehydrate(queryClient);
-  console.log("Server");
-  
 
   return (
     <>
       <Hydrate state={dehydratedState}>
         <InnerTest1 />
       </Hydrate>
-
-      <SyncedWithArticles state={{count: 3}} />
-      <h1 className="my-4 p-3 text-3xl font-bold underline bg-green-100">
-        Articles server synced with client
-      </h1>
-      <InnerTest1 />
     </>
   );
 };
