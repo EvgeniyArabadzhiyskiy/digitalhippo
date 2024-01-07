d98220559a06d4bcead2dce8464e6e6a1f5f5eb9
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-07 17:26:21 +0200

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -36,12 +36,12 @@ const Cart = () => {
       >
         <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {itemCount}
         </span>
       </SheetTrigger>
       <SheetContent className="w-full flex flex-col pr-0 sm:max-w-lg ">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle className="text-green-900">Cart (0)</SheetTitle>
+          <SheetTitle className="text-green-900">Cart ({itemCount})</SheetTitle>
         </SheetHeader>
 
         {itemCount > 0 ? (
