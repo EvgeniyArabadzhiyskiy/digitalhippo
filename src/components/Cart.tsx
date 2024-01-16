d18938f4399ab2985b27bf093c65662509f10523
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-16 21:52:36 +0200

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -19,20 +19,23 @@ import { ScrollArea } from "./ui/scroll-area";
 import CartItem from "./CartItem";
 import { useEffect } from "react";
 import { useCustomStore } from "@/hooks/use-store";
+import { getCartTotal } from "@/lib/helpers/getCartTotal";
 
 const Cart = () => {
   const { items } = useCart();
   console.log("Cart  items:", items);
   const itemCount = items.length;
   const fee = 1;
-  
+
   useEffect(() => {
     useCart.persist.rehydrate();
-  }, [])
+  }, []);
 
-  const cartTotal = items.reduce((total, item) => {
-    return total + item.product.price;
-  }, 0);
+  // const cartTotal = items.reduce((total, item) => {
+  //   return total + item.product.price;
+  // }, 0);
+
+  const cartTotal = getCartTotal(items);
 
   return (
     <Sheet>
