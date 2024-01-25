48b22d086a94d03f19b629ca5f10d7e3e168d7e1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-25 21:44:21 +0200

    Update payload-utils.ts

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -5,23 +5,21 @@ import { NextResponse } from "next/server";
 export const getServerSideUser = async (
   cookies: NextResponse["cookies"] | ResponseCookies
 ) => {
-  const payloadToken = cookies.get("payload-token")?.value || null;
+  // const payloadToken = cookies.get("payload-token")?.value || null;
 
-  const meRes = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
-    {
-      headers: {
-        Authorization: `Bearer ${payloadToken}`,
-      },
-    }
-  );
-
-  const { user, token } = await meRes.json()
+  // const meRes = await fetch(
+  //   `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+  //   {
+  //     headers: {
+  //       Authorization: `JWT ${payloadToken}`,
+  //     },
+  //   }
+  // );
 
   // const { user, token } = (await meRes.json()) as {
   //   user: User | null;
   //   token: string | undefined;
   // };
 
-  return { user, token };
+  // return { user, token };
 };
