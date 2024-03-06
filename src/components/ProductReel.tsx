18b6cb8a63c5abf8f6350d50622ad10df031ab36
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-06 21:23:05 +0200

    Update ProductReel.tsx

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -19,21 +19,29 @@ const FALLBACK_LIMIT = 4;
 const ProductReel = (props: PropsProductReel) => {
   const { title, subtitle, href, query } = props;
   const [lat, setLat] = useState(0)
+  console.log("ProductReel  lat:", lat);
   const [long, setLong] = useState(0)
+  console.log("ProductReel  long:", long);
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLat(position.coords.latitude)
-        setLong(position.coords.longitude)
-        console.log("Latitude: ", position.coords.latitude);
-        console.log("Longitude: ", position.coords.longitude);
-        console.log("Altitude: ", position.coords.altitude);
-      });
+      const watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        },
+        (error) => {
+          console.log("Error getting location:", error);
+        }
+      );
+
+      // return () => {
+      //   navigator.geolocation.clearWatch(watchId);
+      // };
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
-  },[])
+  }, []);
 
   
 
@@ -71,7 +79,7 @@ const ProductReel = (props: PropsProductReel) => {
 
   return (
     <section className="py-12">
-      <h1 className="font-semibold text-2xl">{String(!!navigator.geolocation)}</h1>
+      {/* <h1 className="font-semibold text-2xl">{String(!!navigator.geolocation)}</h1> */}
       <h1 className="font-semibold text-2xl">Latitude: {lat}</h1>
       <h1 className="font-semibold text-2xl">Longitude: {long}</h1>
       <div className="md:flex md:item-center md:justify-between mb-4">
