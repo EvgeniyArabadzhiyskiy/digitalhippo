32c7fe2c25c57ea056f932e7791f0b020d5ee0e0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-07 21:29:46 +0200

    Update LocationTracker.tsx

--- src/components/LocationTracker.tsx
+++ src/components/LocationTracker.tsx
@@ -1,18 +1,50 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+"use client";
+import { trpc } from "@/trpc/client";
+import React, { useEffect, useState } from "react";
+import GoogleMap from "./GoogleMap";
+
+interface ICoords {
+  latitude: number;
+  longitude: number;
+  timestamp: string;
+}
 
 const LocationTracker = () => {
-  const [location, setLocation] = useState<any>(null);
+  const [address, setAddress] = useState<string>("");
+  const [location, setLocation] = useState<ICoords | null>(null);
+  // console.log("LocationTracker  location:", location);
   const [errorMsg, setErrorMsg] = useState<any>(null);
+
+  // const API_KEY= process.env.STRIPE_SECRET_KEY
+  // console.log("GoogleMap  API_KEY:", API_KEY);
+
+  const { mutate: createLocation } = trpc.location.setNewLocation.useMutation({
+    onSuccess: (data) => {
+      setLocation({
+        latitude: data.latitude,
+        longitude: data.longitude,
+        timestamp: data.timestamp,
+      });
+    },
+  });
 
   useEffect(() => {
     const getLocation = () => {
       if (navigator.geolocation) {
         navigator.geolocation.watchPosition(
           (position) => {
-            setLocation({
+            // console.log("getLocation  position:", new Date(position.timestamp));
+
+            // setLocation({
+            //   latitude: position.coords.latitude,
+            //   longitude: position.coords.longitude,
+            //   timestamp: position.timestamp,
+            // });
+
+            createLocation({
               latitude: position.coords.latitude,
               longitude: position.coords.longitude,
+              timestamp: position.timestamp,
             });
           },
           (error) => {
@@ -20,27 +52,46 @@ const LocationTracker = () => {
           }
         );
       } else {
-        setErrorMsg('Geolocation is not supported by this browser.');
+        setErrorMsg("Geolocation is not supported by this browser.");
       }
     };
 
     getLocation();
   }, []);
 
+  const { data: coords } = trpc.location.getCurrentLocation.useQuery();
+  // console.log("LocationTracker  coords:", coords);
+
+  const userLatitude = coords?.length && coords[0].latitude;
+  const userLongitude = coords?.length && coords[0].longitude;
+
   return (
     <div>
+      <ul>
+        {coords?.length &&
+          coords.map((el) => {
+            return (
+              <li key={el.id}>
+                <h1 className="font-semibold text-2xl">
+                  Coordinates: {el.latitude},{el.longitude}
+                </h1>
+                <p className="font-semibold text-2xl">{el.timestamp}</p>
+              </li>
+            );
+          })}
+      </ul>
+
+      {userLatitude && userLongitude && (
+        <GoogleMap
+          location={{ lat: userLatitude, lng: userLongitude }}
+          setAddress={setAddress}
+        />
+      )}
+
+      <h1 className="font-semibold text-2xl">Address: {address}</h1>
       {errorMsg ? <p>{errorMsg}</p> : null}
-      {location ? (
-        
-        <h1 className="font-semibold text-2xl">
-          Latitude: {location.latitude}, Longitude: {location.longitude}
-        </h1>
-      ) : null}
     </div>
   );
 };
 
 export default LocationTracker;
-
-
- 
