3d358a2d8dea0cbc73aed030d7f6c6b7d83f205b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-07 21:30:00 +0200

    Update ProductReel.tsx

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -54,7 +54,7 @@ const ProductReel = (props: PropsProductReel) => {
 
   return (
     <section className="py-12">
-      <LocationTracker />
+      
       <div className="md:flex md:item-center md:justify-between mb-4">
         <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
           {/* <Link href={"/test-1"}>Go Test</Link> */}
