3bf1229583ff66066e9273c17fc99969639dae2e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-07 21:29:28 +0200

    Create page.tsx

---
+++ src/app/location/page.tsx
@@ -0,0 +1,11 @@
+import LocationTracker from "@/components/LocationTracker";
+
+const PageLocation = () => {
+  return (
+    <>
+      <LocationTracker />
+    </>
+  );
+};
+
+export default PageLocation;
